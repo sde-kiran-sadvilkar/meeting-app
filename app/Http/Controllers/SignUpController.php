61fcfc2ad0ed63cdc2252bc5fb5ff8b852ab0037
unknown <sde.kiran@outlook.com>
2025-02-03 22:38:10 +0530

    syntax error

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class SignUpController extends Controller
 {
-    public function signup(Request $request)
+    public function signup(Request $request): JsonResponse
     {
 
         $validator = Validator::make($request->all(), [
